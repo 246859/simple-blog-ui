79a67d084a97f08e027e4cb46109b3e07c1dd889
246859 <2633565580@qq.com>
2023-03-14 16:11:45 +0800

    feat(project): Vite多环境配置

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,34 @@
 import {fileURLToPath, URL} from "node:url";
 
-import {defineConfig} from "vite";
+import {defineConfig, loadEnv} from "vite";
 import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-    plugins: [vue()],
-    resolve: {
-        alias: {
-            "@": fileURLToPath(new URL("./src", import.meta.url))
+export default defineConfig(({command, mode}) => {
+    // 加载环境变量
+    const env = loadEnv(mode, "./env", "");
+    return {
+        base: "/",
+        envDir: "./env",
+        plugins: [vue()],
+        resolve: {
+            alias: {
+                "@": fileURLToPath(new URL("./src", import.meta.url))
+            }
+        },
+        server: {
+            cors: true,
+            port: 7090,
+            proxy: {
+                "/api": {
+                    target: env.VITE_SERVER_API,
+                    changeOrigin: true,
+                    rewrite: path => path.replace(/^\/api/, "")
+                }
+            }
+        },
+        build: {
+            reportCompressedSize: false
         }
-    }
+    };
 });
